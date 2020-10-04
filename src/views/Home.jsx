62bbc034a01c5c1e3a7825d16d206ab4ad4f2cfb
Elena <karengvcs@gmail.com>
2020-10-03 20:31:51 -0500

    Chore: add new file

---
+++ src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Logo from '../assets/images/Logo.png'
+
+const Home = () => {
+  return (
+    <div>
+      <img src={Logo} alt="Logo"/>
+      
+    </div>
+  )
+}
+
+export default Home;
