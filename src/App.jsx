8b4cce0b9d0c8c223704ed8797542d31537d9faf
Elena <karengvcs@gmail.com>
2020-10-05 09:21:52 -0500

    Delete unnecessary dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Home from './views/Home'
+import Characters from './views/Characters'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
       <Route exact path="/" component={Home}/>
+      <Route exact path="/characters" component={Characters}/>
       </Switch>
       
     </BrowserRouter>
