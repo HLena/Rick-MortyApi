85d159a5cec3a4e7d886604cf857264c1f644e1d
Elena <karengvcs@gmail.com>
2020-10-09 22:35:52 -0500

    Add hook to show and hide sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,36 @@
 import React from 'react'
 
+import RadioButton from './RadioButton'
+
 import './styles/Sidebar.scss'
 
-const Sidebar = () => {
+
+
+const Sidebar = (props) => {
+  const {onClick, visible} = props;
   return (
-    <aside>
-      <ul id="filter">
-        <ul id="gender">
-          <h3 className="submenu-title">Gender</h3>
-          <li><label><input type="radio" id="contactChoice2" name="female" value="Female"/>Female</label></li>
-          <li><label><input type="radio" id="contactChoice2" name="male" value="Male"/>Male</label></li>
-          <li><label><input type="radio" id="contactChoice2" name="genderless" value="Genderless"/>Genderless</label></li>
-          <li><label><input type="radio" id="contactChoice2" name="unknown" value="unknown"/>Unknown</label></li>        
-        </ul>
-        <ul id="status">
-          <h3 className="submenu-title">Status</h3>
-          <li><label><input type="radio" id="contactChoice2" name="female" value="Female"/>Alive</label></li>
-          <li><label><input type="radio" id="contactChoice2" name="male" value="Male"/>Dead</label></li>
-          <li><label><input type="radio" id="contactChoice2" name="unknown" value="unknown"/>Unknown</label></li>
-        </ul>
-        <ul id="species">
-          <h3 className="submenu-title">Specie</h3>
-          <li><label><input type="radio" id="contactChoice2" name="unknown" value="human"/>Human</label></li>
-          <li><label><input type="radio" id="contactChoice2" name="unknown" value="unknown"/>Unknown</label></li>
-        </ul>
+    <aside className={visible ? 'show-menu' : ''}>
+      <h2> Filters</h2>
+      <ul id="gender">
+        <h3 className="submenu-title">Gender</h3>
+        <RadioButton onClick={onClick} optionSelected='all' group='gender' />
+        <RadioButton onClick={onClick} optionSelected='female' group='gender' />
+        <RadioButton onClick={onClick} optionSelected='male' group='gender' />
+        <RadioButton onClick={onClick} optionSelected='genderless' group='gender' />
+        <RadioButton onClick={onClick} optionSelected='unknown 1' group='gender' />
+      </ul>
+      <ul id="status">
+        <h3 className="submenu-title">Status</h3>
+        <RadioButton onClick={onClick} optionSelected='all' group='status' />
+        <RadioButton onClick={onClick} optionSelected='alive' group='status' />
+        <RadioButton onClick={onClick} optionSelected='dead' group='status' />
+        <RadioButton onClick={onClick} optionSelected='unknown 2' group='status' />
+      </ul>
+      <ul id="species">
+        <h3 className="submenu-title">Specie</h3>
+        <RadioButton onClick={onClick} optionSelected='all' group='specie' />
+        <RadioButton onClick={onClick} optionSelected='human' group='specie' />
+        <RadioButton onClick={onClick} optionSelected='alien' group='specie' />
       </ul>
     </aside>
   )
