0225cac0de1ddf6ded719eb1360ad6e2a3aa8525
Elena <karengvcs@gmail.com>
2020-10-09 23:14:30 -0500

    Add a div container

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -9,18 +9,19 @@ const Home = () => {
   return (
     <div className="container">
       <img 
-        className="logo mr-5" 
+        className="logo" 
         src={Logo} alt="Logo"/>
-      <div className="description">
-        <p>Ricky and Morty is an American adult animated science diction adventure comedy series creates by Justin Rolland and Dan Harmon for Cartton Network's late-night programming block Adult Swim.</p>
+      <div className="logo-container">
+        <div className="description">
+          <p>Ricky and Morty is an American adult animated science diction adventure comedy series creates by Justin Rolland and Dan Harmon for Cartton Network's late-night programming block Adult Swim.</p>
+        </div>
+        <img 
+          className="main-logo mr-5" 
+          src={MainLogo} alt="Logo"/> 
       </div>
-      <img 
-        className="main-logo mr-5" 
-        src={MainLogo} alt="Logo"/>
       <Link to="/characters">
         <button className="btn primary-btn">Charge Data</button>
       </Link>
-      
     </div>
   )
 }
